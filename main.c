534c9510af6dd6cd0d23752588baac36d49e4f4d
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2024-01-16 16:01:33 -0500

    2

--- main.c
+++ main.c
@@ -9,3 +9,56 @@ struct Alumno {
     char carrera[50];
     float promedio;
 };
+void ingresarAlumnos(struct Alumno *alumnos, int numAlumnos);
+void mostrarAlumnos(struct Alumno *alumnos, int numAlumnos);
+
+int main() {
+    int opcion;
+    int numAlumnos = 0;
+
+    struct Alumno *alumnos = NULL;
+
+    do {
+        printf("\n----- Menú -----\n");
+        printf("1. Ingresar alumnos\n");
+        printf("2. Mostrar información de los alumnos\n");
+        printf("3. Salir\n");
+        printf("Ingrese su opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                printf("\nIngrese el número de alumnos: ");
+                scanf("%d", &numAlumnos);
+
+                alumnos = (struct Alumno *)malloc(numAlumnos * sizeof(struct Alumno));
+
+                if (alumnos == NULL) {
+                    printf("Error de asignación de memoria.\n");
+                    return 1;
+                }
+
+                ingresarAlumnos(alumnos, numAlumnos);
+                break;
+
+            case 2:
+                if (numAlumnos > 0) {
+                    mostrarAlumnos(alumnos, numAlumnos);
+                } else {
+                    printf("Aún no se han ingresado alumnos.\n");
+                }
+                break;
+
+            case 3:
+                free(alumnos);
+                printf("Saliendo del programa. ¡Hasta luego!\n");
+                break;
+
+            default:
+                printf("Opción no válida. Por favor, ingrese una opción válida.\n");
+        }
+
+    } while (opcion != 3);
+
+    return 0;
+}
