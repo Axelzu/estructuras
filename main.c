731c9b87982692571c850a55464fc4ebdad1378a
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2024-01-16 16:01:51 -0500

    3

--- main.c
+++ main.c
@@ -62,3 +62,29 @@ int main() {
 
     return 0;
 }
+void ingresarAlumnos(struct Alumno *alumnos, int numAlumnos) {
+    for (int i = 0; i < numAlumnos; i++) {
+        printf("\nIngrese la información del alumno #%d:\n", i + 1);
+
+        printf("Matrícula: ");
+        scanf("%d", &alumnos[i].matricula);
+
+        
+        while(getchar() != '\n'); // el getchar es para devolver un caracter leido 
+
+        printf("Nombre: ");
+        fgets(alumnos[i].nombre, sizeof(alumnos[i].nombre), stdin);
+        alumnos[i].nombre[strcspn(alumnos[i].nombre, "\n")] = '\0'; //  el fgets nos ayuda a leer los caracteres de la posición actual de ruta hasta el primer carácter de nueva línea , hasta el final de la ruta o hasta que el número de caracteres leídos sea igual a n-1.
+
+        printf("Dirección: ");
+        fgets(alumnos[i].direccion, sizeof(alumnos[i].direccion), stdin); // stdin es como un apuntador para otro archivo es algo similar al getchar
+        alumnos[i].direccion[strcspn(alumnos[i].direccion, "\n")] = '\0'; // sizeof es para poder almacenar o proorcionar una cantidad de almacenamiento 
+
+        printf("Carrera: ");
+        fgets(alumnos[i].carrera, sizeof(alumnos[i].carrera), stdin);
+        alumnos[i].carrera[strcspn(alumnos[i].carrera, "\n")] = '\0';  // el strcspn busca una primera aparicion de un caracter string 1 que pertenece a un conjunto de caracteres especificos por string 2
+
+        printf("Promedio: ");
+        scanf("%f", &alumnos[i].promedio);
+    }
+}
