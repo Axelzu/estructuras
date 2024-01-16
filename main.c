7fc03db75c5a4644045c621ee50c073e015baf99
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2024-01-16 16:00:40 -0500

    1

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Alumno {
+    int matricula;
+    char nombre[50];
+    char direccion[100];
+    char carrera[50];
+    float promedio;
+};
